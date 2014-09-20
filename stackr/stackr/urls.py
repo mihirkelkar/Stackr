166764c84c2475d8aa5f1f3f7ee4f53b32841ebe
mihirkelkar <kelkarmhr@gmail.com>
2014-09-20 08:59:58 -0400

    Making changes to website structure, Adding some database schema. Doing away with some redundant design

--- stackr/stackr/urls.py
+++ stackr/stackr/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+fom django.conf.urls import patterns, include, url
 from views import *
 from django.contrib import admin
 admin.autodiscover()
@@ -10,4 +10,12 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'stackr.views.homepage', name = 'landing'),
+    url(r'^packr/', 'stackr.views.packr', name = 'packr'),
+    url(r'^stackr/', 'stackr.views.stackr', name = 'stackr'),
+    #User Auth Urls
+    url(r'^login/$', 'stackr.views.login', name = "Login Page"),
+    url(r'^auth/$', 'stackr.views.auth_view'),
+    url(r'^/logout', 'stackr.views.logout', name = "Logout Thingy"),
+    url(r'^/loggedin', 'stackr.views.loggedin', name = "Internal landing page"),
+    url(r'^/invalid', 'stackr.views.invalid', name = "Invalid Log In"),
 )
--- stackr/stackr/views.py
+++ stackr/stackr/views.py
@@ -1,5 +1,14 @@
+from django.contrib import auth
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
+from django.core.context_processors import csrf
 
 def homepage(request):
   return render_to_response('home.html')
+
+def packr(request):
+  return render_to_response('packr.html')
+
+def stackr(request):
+  return render_to_response('stackr.html')
